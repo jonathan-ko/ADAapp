e86ab7b4d3fb52a493bd1bd3614d5c235007f620
Jonathan Ko <Jonathan.ko@gmail.com>
2016-10-30 06:47:05 -0700

    working with mongo

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, session
-from flask_pymongo import PyMongo
+import pymongo
 from pymongo import MongoClient
 import json
 	
@@ -7,21 +7,13 @@ from werkzeug import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 Client = MongoClient()
-db = Client.WACDAdb
+db = Client.WACDA
 users = db.users
+contacts = db.contacts
+access = db.access
+places = db.places
 
 app.secret_key = 'why would I tell you my secret key?'
-
-
-#mysql = MySQL()
-	 
-# MySQL configurations
-#app.config['MYSQL_DATABASE_USER'] = 'oliver'
-#app.config['MYSQL_DATABASE_PASSWORD'] = 'holmes'
-#app.config['MYSQL_DATABASE_DB'] = 'contacts'
-#app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-#mysql.init_app(app)
-
 
 @app.route("/")
 def main():
@@ -38,21 +30,13 @@ def showSignUp():
 @app.route('/signUp', methods=['POST'])
 def signUp():
     # read the posted values from the UI
-    Last_name = request.form['inputLastName']
-    First_name = request.form['inputFirstName']
     email = request.form['inputEmail']
     password = request.form['inputPassword']
     # validate the received values
-    if Last_name and First_name and email and password:
+    if email and password:
     	hashed_password = generate_password_hash(password)
-        #conn = mysql.connect()
-        #cursor = conn.cursor()
-        #cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
-        #data = cursor.fetchall()
-        #if len(data) is 0:
-            #conn.commit()
-        if users.count({"Email": email}) == 0:
-            users.insert_one({"Last_name": Last_name, "First_name": First_name, "Email": email, "hashed_password": hashed_password})
+        if users.count({"email": email}) == 0:
+            users.insert_one({"email": email, "hashed_password": hashed_password})
             return json.dumps({'message':'User created successfully !'})
         else:
             return json.dumps({'error':'<span>Email exists!</span>'})
@@ -68,9 +52,9 @@ def validateLogin():
     try:
         username = request.form['inputEmail']
         password = request.form['inputPassword']
-        data = users.find_one({"Email":username})
+        data = users.find_one({"email":username})
         if check_password_hash(data['hashed_password'],password):
-            session['user'] = data['Email']
+            session['user'] = data['email']
             return redirect('/dashboard')
         else:
             return render_template('error.html',error = 'Wrong Email address or Password.') 
@@ -84,67 +68,45 @@ def dashboard():
     else:
         return render_template('error.html',error = 'Unauthorized Access')
 
-@app.route('/clients')
-def clients():
+@app.route('/showClients')
+def getClientList():
     return render_template('clients.html')
 
 @app.route('/addClient', methods=['POST'])
 def addClient():
-    # read the posted values from the UI
-    _LastName = request.form['inputLastName']
-    _FirstName = request.form['inputFirstName']
-    _Address = request.form['inputAddress']
-    _City = request.form['inputCity']
-    _ZipCode = request.form['inputZipCode']
-    _Notes = request.form['inputNotes']
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    cursor.callproc('sp_addClient',(_LastName,_FirstName,_Address,_City,_ZipCode,_Notes))
-    data = cursor.fetchall()
-    if len(data) is 0:
-        conn.commit()
-        cursor.close()
-        conn.close()
-        return redirect('/clients')
-    else:
-        cursor.close()
-        conn.close()
-        return render_template('error.html',error = 'An error occurred!')
+    try:
+        if session.get('user'):
+            email = session.get('user')
+            user_obj=users.find_one({"email":email})
+            user_id=user_obj._id
+            print user_id
+
+            # read the posted values from the UI
+            last_name = request.form['inputLastName']
+            first_name = request.form['inputFirstName']
+            address = request.form['inputAddress']
+            city = request.form['inputCity']
+            zipcode = request.form['inputZipCode']
+            notes = request.form['inputNotes']
+            contacts.insert_one({"last_name":last_name, "first_name":first_name, "address":address, "city":city, "zipcode":zipcode, "notes":notes, "user_id":user_id})
+            return redirect('/showClients')
+        else:
+            return redirect('signin.html')
+    except:
+        return render_template('error.html', error = str(e))
 
 @app.route('/getClient')
 def getClient():
-    try:
-        if session.get('user'):
-            _user = session.get('user')
+    if session.get('user'):
+        email = session.get('user')
+        user_obj=users.find_one({"email":email})
+        print user_obj        
+        user_id=user_obj['_id']            
+        print user_id
+        return json.dumps({"data":str(user_id)})
+    else:
+        return render_template('error.html', error = 'Unauthorized Access')
 
-            # Connect to MySQL and fetch data
-            con = mysql.connect()
-            cursor = con.cursor()
-            cursor.callproc('sp_getClient')
-            clients = cursor.fetchall()
-            print clients
-            clients_dict = []
-            for client in clients:
-                print client
-                client_dict = {
-                    'client_id': client[0],
-                    'client_LastName': client[1],
-                    'client_FirstName': client[2],
-                    'client_Address': client[3],
-                    'client_City': client[4],
-                    'client_Zipcode': client[5],
-                    'client_Notes': client[6],
-                    'client_user_id': client[7],
-                    'client_email': client[8],
-                    'client_caseStatus': client[9],
-                    'client_caseType': client[10]
-                }
-                clients_dict.append(client_dict)
-            return json.dumps(clients_dict)
-        else:
-            return render_template('error.html', error = 'Unauthorized Access')
-    except Exception as e:
-        return render_template('error.html', error = str(e))
 
 @app.route('/logout')
 def logout():
@@ -159,11 +121,7 @@ def testDash():
 @app.route('/getClients')
 def getClients():
     try:
-        # Connect to MySQL and fetch data
-        con = mysql.connect()
-        cursor = con.cursor()
-        cursor.callproc('sp_getClient')
-        clients = cursor.fetchall()
+        email = session['user']
         print clients
         clients_dict = []
         for client in clients:
