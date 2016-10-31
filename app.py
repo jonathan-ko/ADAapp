1e5d415b23e055197cc816e0a8daea9f3b23ce12
Jonathan Ko <Jonathan.ko@gmail.com>
2016-10-31 13:40:29 -0700

    added uploads

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request, redirect, session
+import os
+from flask import Flask, render_template, request, redirect, session, url_for, send_from_directory
 import pymongo
 from pymongo import MongoClient
 import json
 	
-from werkzeug import generate_password_hash, check_password_hash
+from werkzeug import generate_password_hash, check_password_hash, secure_filename
+
+
 
 app = Flask(__name__)
 Client = MongoClient()
@@ -14,6 +17,41 @@ access = db.access
 places = db.places
 
 app.secret_key = 'why would I tell you my secret key?'
+
+# This is the path to the upload directory
+app.config['UPLOAD_FOLDER'] = 'static/upload/'
+# These are the extension that we are accepting to be uploaded
+app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+# For a given file, return whether it's an allowed type or not
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+# Route that will process the file upload
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+    # Get the name of the uploaded file
+        file = request.files['file']
+        # Check if the file is one of the allowed types/extensions
+        if file and allowed_file(file.filename):
+            # Make the filename safe, remove unsupported chars
+            filename = secure_filename(file.filename)
+            # Move the file form the temporal folder to
+            # the upload folder we setup
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # Redirect the user to the uploaded_file route, which
+            # will basicaly show on the browser the uploaded file
+            return redirect(url_for('uploaded_file', filename=filename))
+
+# This route is expecting a parameter containing the name
+# of a file. Then it will locate that file on the upload
+# directory and show it on the browser, so if the user uploads
+# an image, that image is going to be show after the upload
+@app.route('/static/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 @app.route("/")
 def main():
@@ -26,6 +64,10 @@ def about():
 @app.route('/showSignUp')
 def showSignUp():
     return render_template('signup.html')
+
+@app.route('/showTestUpload')
+def testUpload():
+    return render_template('testUpload.html')
 
 @app.route('/signUp', methods=['POST'])
 def signUp():
@@ -67,6 +109,10 @@ def dashboard():
         return render_template('dashboard.html')
     else:
         return render_template('error.html',error = 'Unauthorized Access')
+
+@app.route('/showPlaces')
+def getPlaces():
+    return render_template('addPlace.html')
 
 @app.route('/showClients')
 def getClientList():
